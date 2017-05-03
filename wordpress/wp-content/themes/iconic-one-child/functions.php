28aabcc4dfd5362237ed003b5e43d581ac4bab7b
Yo <yohanndore48@gmail.com>
2017-05-03 16:45:48 +0200

    Add pictures to main menu

--- wordpress/wp-content/themes/iconic-one-child/functions.php
+++ wordpress/wp-content/themes/iconic-one-child/functions.php
@@ -5,4 +5,11 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );}
+
+function mail_send() {
+    mail('yohanndore48@gmail.com', 'rien', 'c\'est bon ça marche');
+}
+
+add_action("save_post", "mail_send");
+
 ?>
